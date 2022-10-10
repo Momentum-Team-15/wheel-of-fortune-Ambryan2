2f6cc8f7ff8f9256bd1e30bc7f3a3a034033cf65
Your Name <alobryan2@gmail.com>
2022-10-10 11:40:20 -0400

    did nothing but planning

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,25 @@
 def play_game():
-    pass
+    #TODO need to select the word from the file
+    w = open('test-word.txt', "r")
+    p = w.read()
+
+    #TODO Make the words from file into a list
+    filtered = p.translate(str.maketrans('\n', ' '))
+    items = filtered.split()
+
+    # this picks first item in list will edit later
+    desire = items[0]  #this equals dream right now 
+    
+    # this is what is used to determine if player is guessed
+    
+    #TODO need to make a board with the spaces for the length of the word
+    
+    #TODO Need to make a loop that ask people to select a letter (case of letter shouldn't matter) 
+    #TODO Should be a while loop the ends when user guesses word or when they run out of guesses
+    
+
+    #TODO In loop I need it to tell when a letter was selected 
+    #TODO  
 
 
 if __name__ == "__main__":
